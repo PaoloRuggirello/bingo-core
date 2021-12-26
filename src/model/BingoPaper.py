5fae8ed6e8443a86f654b50358caa26763c6b77c
PaoloRuggirello <ruggirello99@hotmail.it>
2021-12-26 10:34:05 +0100

    Added model package with Card and BingoPaper calsses. Added problem description in BINGO_REQUIREMENTS.md file. Updated requirements.txt

---
+++ src/model/BingoPaper.py
@@ -0,0 +1,14 @@
+from Card import Card
+
+
+class BingoPaper:
+
+    def __init__(self):
+        self.cards = self.generate_cards()
+
+
+    def generate_cards(self):  # Each bingo paper must contain number from 1 to 90 without repetitions
+        cards = []
+        for i in range(1, 7):
+            cards.append(Card())
+        return cards
---
+++ src/model/Card.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+class Card:
+
+    def __init__(self):
+        self.rows = np.zeros((3, 9), dtype=int)
