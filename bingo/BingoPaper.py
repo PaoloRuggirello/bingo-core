21fb32a99de2ab9068d0f3f2c9cfb40d896b1f8e
PaoloRuggirello <ruggirello99@hotmail.it>
2021-12-28 11:38:54 +0100

    Random generation of numbers for each card is now ok

--- bingo/BingoPaper.py
+++ bingo/BingoPaper.py
@@ -1,7 +1,8 @@
 from bingo.Card import Card
 from bingo.Utils import PAPER_NUMBERS
-from random import randrange, choice
+from random import choice
 import numpy as np
+from bingo.Utils import np_pop
 
 
 class BingoPaper:
@@ -12,47 +13,78 @@ class BingoPaper:
 
     def generate_cards(self):  # Each bingo paper must contain number from 1 to 90 without repetitions
         cards = []
-        for i in range(1, 7):
-            card_numbers = self.get_card_numbers(missing_cards=6-i)
-            cards.append(Card(i, card_numbers))
+        for card_id, card_numbers in enumerate(self.generate_cards_numbers()):
+            cards.append(Card(card_id + 1, card_numbers))
         return cards
 
-    def get_card_numbers(self, missing_cards):
-        card = np.zeros((3, 9), dtype=int)
+    def generate_cards_numbers(self):
+        cards = []
+        for _ in range(6):  # Filled one number per unit in each card
+            card = np.zeros((3, 9), dtype=int)
+            self.set_number_per_unit(card)
+            cards.append(card)
+
+        while len(self.paper_cards_numbers) > 0:
+            number, self.paper_cards_numbers = np_pop(self.paper_cards_numbers)
+            column_index = self.get_column_index(number)
+
+            # Wants to know which cards can store the number
+            available_cards = self.get_cards_with_available_column(cards, column_index)
+            if available_cards:
+                chosen_card_tuple = choice(available_cards)  # Chosen_card is a tuple
+            else:
+                available_cards = self.get_cards_with_available_column(cards, column_index, False)
+                chosen_card_tuple = choice(available_cards)
+                random_number = int(self.get_random_number_from_second_third_rows(chosen_card_tuple[0]))
+                self.paper_cards_numbers = np.append(self.paper_cards_numbers, random_number)
+                chosen_card_tuple[0][chosen_card_tuple[0] == random_number] = 0
+
+            chosen_card = chosen_card_tuple[0]
+            chosen_card_free_index = chosen_card_tuple[1]
+
+            chosen_card[chosen_card_free_index, column_index] = number
+
+        return cards
+
+    def set_number_per_unit(self, card):
         for i in range(9):
             possible_indexes_for_range = self.get_indexes_in_range_unit(i)
             random_index = choice(possible_indexes_for_range)
             card[0, i] = self.paper_cards_numbers[random_index]
             self.paper_cards_numbers = np.delete(self.paper_cards_numbers, random_index)
 
-        i = 0
-        while i < 6:  # Each card has 15 numbers
-            random_index = randrange(self.paper_cards_numbers.size)
-            column_index = self.get_column_index(random_index)
-            if len(self.get_indexes_in_range_unit(column_index)) > missing_cards:
-                column = card[:, column_index]
-                numbers_in_column = len(column[column > 0])
-                if numbers_in_column < 3:
-                    card[numbers_in_column, column_index] = self.paper_cards_numbers[random_index]
-                    self.paper_cards_numbers = np.delete(self.paper_cards_numbers, random_index)
-                    i += 1
-
-        return card
-
-    def get_column_index(self, random_index):
-        column_index = int(self.paper_cards_numbers[random_index] / 10)
-        column_index = column_index - 1 if column_index == 9 else column_index
-        return column_index
-
     def get_indexes_in_range_unit(self, range_unit):
         indexes = []
         for number_index, number in enumerate(self.paper_cards_numbers):
             if range_unit < 8:
-                if int(number/10) == range_unit:
+                if int(number / 10) == range_unit:
                     indexes.append(number_index)
             else:
-                if int(number/10) == range_unit or int(number/10) == range_unit + 1:
+                if int(number / 10) == range_unit or int(number / 10) == range_unit + 1:
                     indexes.append(number_index)
         return indexes
 
+    @staticmethod
+    def get_column_index(number):
+        column_index = int(number / 10)
+        column_index = column_index - 1 if column_index == 9 else column_index
+        return column_index
+
+    @staticmethod
+    def get_cards_with_available_column(all_cards, column_index, check_total_numbers=True):
+        available_cards = []
+        for card in all_cards:
+            total_numbers = np.count_nonzero(card)
+            column = card[:, column_index]
+            numbers_in_column = len(column[column > 0])  # Equals to first available row index
+            if (total_numbers < 15 or not check_total_numbers) and numbers_in_column < 3:
+                available_cards.append((card, numbers_in_column))
+        return available_cards
+
+    @staticmethod
+    def get_random_number_from_second_third_rows(card):
+        temp_card = np.zeros((2, 9))
+        for i in range(1, 3):
+            temp_card[i-1, :] = card[i, :]
+        return choice(temp_card[temp_card > 0])
 
--- bingo/Utils.py
+++ bingo/Utils.py
@@ -1,3 +1,9 @@
 import numpy as np
 
 PAPER_NUMBERS = np.arange(90) + 1
+
+
+def np_pop(np_array):
+    number = np_array[0]
+    np_array = np.delete(np_array, 0)
+    return number, np_array
