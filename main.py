37709cc3bfffa7e7835b60c67f329d136820ed40
Simone Pio Caronia <spcaronia@gmail.com>
2021-12-30 14:37:51 +0100

    creation of players and cards linked

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from bingo import Utils
 from bingo.base_model.BaseBingoPaper import BaseBingoPaper
+from bingo.base_model.BaseUser import BaseUser
+
+
+def get_users_with_cards_and_remove_unused_cards(bingo_papers_for_users, n_cards_per_user) -> list:
+    users = []
+    paper_index = card_index = 0
+    for index_user, n_cards in enumerate(n_cards_per_user):
+        cards_user = []
+        for _ in range(n_cards):
+            if card_index > 5:
+                card_index = 0
+                paper_index += 1
+            cards_user.append(bingo_papers_for_users[paper_index].cards[card_index])
+            card_index += 1
+        users.append(BaseUser(f'Player{index_user + 1}', cards=cards_user))
+    bingo_papers_for_users[paper_index].cards = bingo_papers_for_users[paper_index].cards[:card_index]
+    return users
+
 
 if __name__ == '__main__':
     print("Bingo-core online")
@@ -13,6 +31,9 @@ if __name__ == '__main__':
             print(f"Number of gamers ({GAMERS}) doesn't"
                   f' match length of number of cards ({len(N_CARDS)})')
             exit()
+        elif GAMERS < 1:
+            print(f"Number of gamers ({GAMERS}) is less than 1!")
+            exit()
     except ValueError as e:
         print(e)
         exit()
@@ -22,3 +43,5 @@ if __name__ == '__main__':
 
     for i in range(number_of_papers_for_gamers):
         bingo_papers.append(BaseBingoPaper(id_paper=i+1))
+
+    USERS = get_users_with_cards_and_remove_unused_cards(bingo_papers[1:], N_CARDS)
