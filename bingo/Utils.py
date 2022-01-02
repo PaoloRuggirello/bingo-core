5c6b4f7d50f6eace96526b73593b7658256eb664
PaoloRuggirello <ruggirello99@hotmail.it>
2022-01-02 13:01:46 +0100

    Added config to suppress warning message

--- bingo/Utils.py
+++ bingo/Utils.py
@@ -6,6 +6,7 @@ import random, string, argparse
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql@localhost/bingo_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 PAPER_NUMBERS = np.arange(90) + 1
