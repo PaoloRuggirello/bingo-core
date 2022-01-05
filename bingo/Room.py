0f12690b899237768ac9337f8e78aaf33a9dc99a
PaoloRuggirello <ruggirello99@hotmail.it>
2022-01-05 10:22:39 +0100

    Completed service extract numbers

--- bingo/Room.py
+++ bingo/Room.py
@@ -9,7 +9,7 @@ class Room(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     code = db.Column('code', db.String(5), unique=True)
     name = db.Column('name', db.String(20))
-    extracted_numbers = db.Column('extracted_numbers', NPArray, nullable=True)
+    extracted_numbers = db.Column('extracted_numbers', NPArray(), nullable=True)
     unique_code = db.Column('unique_code', db.String(6))
     papers = relationship("BingoPaper")
     users = relationship("User")
@@ -18,4 +18,3 @@ class Room(db.Model):
         self.name = name
         self.code = get_random_room_code()
         self.unique_code = unique_code
-        self.extracted_numbers = np.empty(shape=0, dtype=int)
